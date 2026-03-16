51e6da1535c7ec1a01a0bb07728f22bf9b8ebc7e
Sunth <2324802010131@student.tdmu.edu.vn>
2026-03-16 11:17:18 +0700

    Hoàn thành Task 2 và thêm file backup CSDL

--- ProductService.Api/Program.cs
+++ ProductService.Api/Program.cs
@@ -1,3 +1,4 @@
+using MediatR;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
